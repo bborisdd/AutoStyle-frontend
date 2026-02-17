fcabf05f54bd52316b542c2be44b76662912e7c8
Белоконев Г.С. <georgibelok@gmail.com>
2026-02-17 08:45:02 +0300

    fix commit

--- js/main.js
+++ js/main.js
@@ -474,7 +474,12 @@ function showLoginModal() {
                     <label class="form-label" for="loginPassword">Пароль *</label>
                     <div class="relative">
                         <input type="password" id="loginPassword" class="form-input pr-12" placeholder="Введите пароль">
-                        
+                        <button type="button" class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700" onclick="togglePasswordVisibility('loginPassword', this)">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                            </svg>
+                        </button>
                     </div>
                     <p id="loginPasswordError" class="text-red-500 text-sm mt-1 hidden"></p>
                 </div>
@@ -546,9 +551,9 @@ function initLoginFormValidation() {
         clearFieldError('loginEmail');
     });
     
-    passwordInput.addEventListener('input', function() {
-        clearFieldError('loginPassword');
-    });
+    // passwordInput.addEventListener('input', function() {
+    //     clearFieldError('loginPassword');
+    // });
     
     // Обработка отправки формы
     form.addEventListener('submit', function(e) {
