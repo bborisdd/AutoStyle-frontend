41d85e9dcec913c647150398e2e7180aea8e8107
Белоконев Г.С. <georgibelok@gmail.com>
2026-02-17 08:40:33 +0300

    paswordfix commit

--- js/main.js
+++ js/main.js
@@ -21,25 +21,25 @@ document.addEventListener('DOMContentLoaded', function() {
 /**
  * Переключение видимости пароля
  */
-// function togglePasswordVisibility(inputId, button) {
-//     const input = document.getElementById(inputId);
-//     const svg = button.querySelector('svg');
+function togglePasswordVisibility(inputId, button) {
+    const input = document.getElementById(inputId);
+    const svg = button.querySelector('svg');
     
-//     if (input.type === 'password') {
-//         input.type = 'text';
-//         // Иконка "скрыть" (перечёркнутый глаз)
-//         svg.innerHTML = `
-//             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
-//         `;
-//     } else {
-//         input.type = 'password';
-//         // Иконка "показать" (глаз)
-//         svg.innerHTML = `
-//             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-//             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-//         `;
-//     }
-// }
+    if (input.type === 'password') {
+        input.type = 'text';
+        // Иконка "скрыть" (перечёркнутый глаз)
+        svg.innerHTML = `
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"></path>
+        `;
+    } else {
+        input.type = 'password';
+        // Иконка "показать" (глаз)
+        svg.innerHTML = `
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+        `;
+    }
+}
 
 /**
  * Мобильное меню
@@ -474,12 +474,7 @@ function showLoginModal() {
                     <label class="form-label" for="loginPassword">Пароль *</label>
                     <div class="relative">
                         <input type="password" id="loginPassword" class="form-input pr-12" placeholder="Введите пароль">
-                        <button type="button" class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700" onclick="togglePasswordVisibility('loginPassword', this)">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                            </svg>
-                        </button>
+                        
                     </div>
                     <p id="loginPasswordError" class="text-red-500 text-sm mt-1 hidden"></p>
                 </div>
